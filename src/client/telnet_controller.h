2963c13b8dabb3a5dd4343c327fc04ac65101aee
syzymon <s.tworkowski@student.uw.edu.pl>
2020-06-06 16:43:12 +0200

    Implement auto-refresh in telnet menu (poll)

--- src/client/telnet_controller.h
+++ src/client/telnet_controller.h
@@ -1,6 +1,7 @@
 #ifndef RADIO_SK_SRC_CLIENT_TELNET_CONTROLLER_H_
 #define RADIO_SK_SRC_CLIENT_TELNET_CONTROLLER_H_
 
+#include <poll.h>
 #include "../utils/tcp.h"
 #include "io.h"
 #include "menu.h"
@@ -10,13 +11,30 @@ using __gnu_cxx::stdio_filebuf;
 class Controller {
   tcp::TCPServer server_;
   menu::Menu menu_;
+  pollfd pollfd_{};
+  static constexpr size_t POLL_TIMEOUT_ON_KEY = 69;
+
+  io::KeyType wait_for_key(int sock) {
+    while (true) {
+      io::TelnetIOHandler::display_lines(sock, menu_.render_lines());
+
+      pollfd_.events = POLLIN;
+      int ret = poll(&pollfd_, 1, POLL_TIMEOUT_ON_KEY);
+      switch (ret) {
+        case -1:throw exceptions::SocketException(errno);
+        case 0:break;
+        default:return io::TelnetIOHandler::get_key(sock);
+      }
+    }
+  }
 
   [[noreturn]] void handle_one_connection(int sock) {
+    pollfd_.fd = sock;
+
     io::TelnetIOHandler::negotiate(sock);
     while (true) {
-      auto lines = menu_.render_lines();
-      io::TelnetIOHandler::display_lines(sock, lines);
-      auto msg = io::TelnetIOHandler::get_key(sock);
+      auto msg = wait_for_key(sock);
+
       switch (msg) {
         case io::KEY_UP:menu_.on_down();
           break;
