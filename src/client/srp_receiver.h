23cfa6bbc7be2f3f7154159c8952fedc88f39c3b
syzymon <s.tworkowski@student.uw.edu.pl>
2020-06-06 13:14:33 +0200

    Update srp::Receiver

--- src/client/srp_receiver.h
+++ src/client/srp_receiver.h
@@ -3,16 +3,19 @@
 
 #include <iostream>
 
-#include "utils/sik_radio_protocol.h"
-#include "utils/receiver.h"
+#include "../utils/sik_radio_protocol.h"
+#include "../utils/receiver.h"
+#include "../utils/addr.h"
+#include "stdout_metadata_proxy.h"
 #include "proxies_handler_socket.h"
 
 namespace srp {
 class Receiver : public AbstractReceiver {
-  ProxiesHandlerSocket &client;
-  const types::addr_t &proxy_addr;
+  ProxiesHandlerSocket &client_;
+  const types::addr_t &proxy_addr_;
+  State &state_;
 
-  void dispatch_msg(const srp::Message &msg, types::addr_t to) {
+  void dispatch_msg(const srp::Message &msg) {
     switch (msg.type()) {
       case srp::AUDIO:on_audio(msg.body());
         break;
@@ -23,24 +26,28 @@ class Receiver : public AbstractReceiver {
 
   [[noreturn]] void listen() {
     while (true) {
-      auto incoming_msg = client.get_msg();
+      auto incoming_msg = client_.get_msg();
       auto req = srp::Message::decode(incoming_msg.first);
+      auto current_client_addr = state_.get_current_client_addr();
+
+//      std::cerr << "MSG: " << req.type() << "PROXY: " << (current_client_addr != std::nullopt) << '\n';
 
       if (req.type() == srp::IAM) {
-        std::cerr << "I AM";
-        // TODO:
-      } else {
-        dispatch_msg(req, incoming_msg.second);
+        state_.add_proxy(incoming_msg.second, req.body());
+      } else if (current_client_addr && addr::addr_equal(incoming_msg.second, *current_client_addr)) {
+        state_.set_last_msg_time();
+        dispatch_msg(req);
       }
+      // TODO: do not send keepalive in this thread
       auto resp = srp::Message(srp::KEEPALIVE);
-      client.send_msg({resp.encode(), proxy_addr});
+      client_.send_msg({resp.encode(), proxy_addr_});
     }
   }
 
  public:
-  Receiver(proxy::StdoutProxy &proxy,
+  Receiver(proxy::StdoutMetadataProxy &proxy, State &state,
            ProxiesHandlerSocket &client, const types::addr_t &proxy_addr) :
-      AbstractReceiver(proxy), client(client), proxy_addr(proxy_addr) {}
+      AbstractReceiver(proxy), client_(client), proxy_addr_(proxy_addr), state_(state) {}
 
   [[noreturn]] void operator()() override {
     listen();
