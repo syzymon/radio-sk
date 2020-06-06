0af7d3cfdefe72f79358c8866e0ed0db87512a77
syzymon <s.tworkowski@student.uw.edu.pl>
2020-06-06 15:35:57 +0200

    Add TTL for multicast in client

--- src/client/proxies_handler_socket.h
+++ src/client/proxies_handler_socket.h
@@ -7,10 +7,14 @@ static constexpr size_t BUF_SIZE = 65536;
 
 class ProxiesHandlerSocket : public udp::UDPClient<BUF_SIZE> {
   using super = udp::UDPClient<BUF_SIZE>;
+  static constexpr int TTL_VALUE = 4;
 
  public:
   ProxiesHandlerSocket() {
     init_bcast();
+    int optval = TTL_VALUE;
+    if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, (void *) &optval, sizeof optval) < 0)
+      throw exceptions::SocketException(errno);
   }
 };
 
